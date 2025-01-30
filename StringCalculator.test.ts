6cd6450400dfe368424b1dfe9da460d0145668f2
Neha Verma <neha.verma@cygnetinfotech.com>
2025-01-30 16:30:57 +0530

    Refactored the code

--- StringCalculator.test.ts
+++ StringCalculator.test.ts
@@ -9,31 +9,36 @@ describe('StringCalculator', () => {
 
     // Empty string test
     test('Must 0 for an empty string', () => {
-    expect(stringCalculator.add('')).toBe(0);
+    expect(stringCalculator.main('')).toBe(0);
     });
 
     // Single number in string test
     test('Must return the number itself for a single number', () => {
-    expect(stringCalculator.add('2')).toBe(2);
+    expect(stringCalculator.main('2')).toBe(2);
     });
 
     // Two numbers in string test
     test('Must return the sum of two numbers', () => {
-    expect(stringCalculator.add('3,4')).toBe(7);
+    expect(stringCalculator.main('3,4')).toBe(7);
     });
 
     // Multiple numbers in string test
     test('Must return the sum of multiple numbers', () => {
-    expect(stringCalculator.add('3,2,6')).toBe(11);
+    expect(stringCalculator.main('3,2,6')).toBe(11);
     });
 
     // Multiple numbers with \n should also return sum
     test('Must return the sum of multiple numbers', () => {
-    expect(stringCalculator.add('1\n2,3')).toBe(6);
+    expect(stringCalculator.main('1\n2,3')).toBe(6);
     });
 
     // Multiple numbers with delimiter format //[delimiter]\n[numbers…] should also return sum
     test('Must return the sum of multiple numbers', () => {
-    expect(stringCalculator.add('//;\n1;2')).toBe(3);
+    expect(stringCalculator.main('//;\n1;2')).toBe(3);
     });
+
+    // Multiple numbers with one or more negative values should return error
+    // test('Must throw error with details of negative numbers', () => {
+    // expect(stringCalculator.main('1,-2,-3,4')).toThrow('Negatives not allowed');
+    // });
 });
--- StringCalculator.ts
+++ StringCalculator.ts
@@ -1,24 +1,69 @@
 export class StringCalculator {
-    // Method to add numbers from a string
+    /**
+     * Method to add numbers from a string
+     * 
+     * @param numbers The number string
+     * 
+     */
     public add(numbers: string): number {
       if (!numbers) return 0; // If the input is an empty string, return 0.
-
+      
+      // Split the input string by commas or \n and convert to an array of numbers.
       let numberArray: any[] = [];
+      numberArray = numbers.split(/[\n,]+/).map(n => parseInt(n.trim(), 10));      
+
+      // Return the sum of the numbers.
+      console.log(numberArray);
+      return numberArray.reduce((sum, current) => sum + current, 0);
+    }
+
+    public throwError(message: string): never {
+        throw new Error(message);
+    }
+  
+    /**
+     * Function to check if delimiter present
+     * 
+     * @param numbers The number string
+     * 
+     */
+    public hasDelimiter(numbers: string): boolean {
+      if (numbers.includes('//')) {
+          return true;
+      } else {
+         return false;
+      }
+    }    
+
+    /**
+     * Method to add numbers from a string having delimiter
+     * 
+     * @param numbers The number string
+     * 
+     */
+    public addWithDelimiter(numbers: string): number {
+      // Remove occurrences of '//' from the string
+      let numbersStr = numbers.replace('//', '');
 
       // Find delimiter and filter it to get number string with that delimiter only
-      if (numbers.includes('//')) {
-          // Remove occurrences of '//' from the string
-          let numbersStr = numbers.replace('//', '');
-          const tempArray = numbersStr.split(/[\n]+/).map(n => (n.trim()));
-          numberArray = tempArray[1].split(tempArray[0]).map(n => parseInt(n.trim(), 10));
-      } else {
-          // Split the input string by commas convert to an array of numbers.
-          numberArray = numbers.split(/[\n,]+/).map(n => parseInt(n.trim(), 10));
-      }
-
+      let numberArray: any[] = [];
+      const tempArray = numbersStr.split(/[\n]+/).map(n => (n.trim()));
+      numberArray = tempArray[1].split(tempArray[0]).map(n => parseInt(n.trim(), 10));
       console.log(numberArray);
-      // Return the sum of the numbers.
       return numberArray.reduce((sum, current) => sum + current, 0);
+    }
+
+    /**
+     * Main funtion to process add on numbers strings
+     * 
+     * @param numbers The number string
+     * 
+     */
+    public main(numbers: string): number {
+      if (this.hasDelimiter(numbers)) {
+          return this.addWithDelimiter(numbers);
+      }
+      return this.add(numbers);
     }
 }
 
