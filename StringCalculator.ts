c98db57325c617e86531f720d11fd14c4516da99
Neha Verma <neha.verma@cygnetinfotech.com>
2025-01-30 13:09:12 +0530

    Feat: Modified add method to handle new lines between numbers

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -4,7 +4,7 @@ export class StringCalculator {
       if (!numbers) return 0; // If the input is an empty string, return 0.
 
       // Split the input string by commas convert to an array of numbers.
-      const numberArray = numbers.split(/[,]+/).map(n => parseInt(n.trim(), 10));
+      const numberArray = numbers.split(/[\n,]+/).map(n => parseInt(n.trim(), 10));
 
       // Return the sum of the numbers.
       return numberArray.reduce((sum, current) => sum + current, 0);
