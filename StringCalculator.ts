fcd7297bbe6bcb939e590e73a3e8e4b32fea46f3
Neha Verma <neha.verma@cygnetinfotech.com>
2025-01-30 13:45:28 +0530

    Feat: Modified add method to handle delimiter logic

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -3,9 +3,20 @@ export class StringCalculator {
     public add(numbers: string): number {
       if (!numbers) return 0; // If the input is an empty string, return 0.
 
-      // Split the input string by commas convert to an array of numbers.
-      const numberArray = numbers.split(/[\n,]+/).map(n => parseInt(n.trim(), 10));
+      let numberArray: any[] = [];
 
+      // Find delimiter and filter it to get number string with that delimiter only
+      if (numbers.includes('//')) {
+          // Remove occurrences of '//' from the string
+          let numbersStr = numbers.replace('//', '');
+          const tempArray = numbersStr.split(/[\n]+/).map(n => (n.trim()));
+          numberArray = tempArray[1].split(tempArray[0]).map(n => parseInt(n.trim(), 10));
+      } else {
+          // Split the input string by commas convert to an array of numbers.
+          numberArray = numbers.split(/[\n,]+/).map(n => parseInt(n.trim(), 10));
+      }
+
+      console.log(numberArray);
       // Return the sum of the numbers.
       return numberArray.reduce((sum, current) => sum + current, 0);
     }
